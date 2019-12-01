728c9da4caf896d5b72c430aae8a0981c12d532a
Sigmaflo2 <kjschocolate@gmail.com>
2019-12-01 17:51:56 +0900

    show movielist on screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import PropTypes from "prop-types";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component{
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
   };
   getMovies = async () => {
     const {
       data: {
         data: { movies }
       }
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    console.log(movies);
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({movies, isLoading: false});
   };
   componentDidMount(){
     this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading" : "We are ready"}</div>
+    const { isLoading, movies } = this.state;
+    return <div>{isLoading ? "Loading..."
+    : movies.map(movie => {
+      console.log(movie);
+      return(
+         <Movie
+           id={movie.id}
+           year={movie.year}
+           title={movie.title}
+           summary={movie.summary}
+           poster={movie.medium_cover_image}
+          />
+        );
+    })}
+    </div>
   }
 }
 
